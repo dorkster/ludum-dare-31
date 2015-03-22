bd0db87f3a4e1620d86803df9f5981beeb4bac5f
Justin Jacobs <jajdorkster@gmail.com>
2015-03-22 15:42:59 -0400

    License under GPL v3

--- src/enemy.h
+++ src/enemy.h
@@ -1,3 +1,21 @@
+/*
+Copyright © 2014-2015 Justin Jacobs
+
+This file is part of Noman's Dungeon.
+
+Noman's Dungeon is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Noman's Dungeon is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Noman's Dungeon.  If not, see http://www.gnu.org/licenses/
+*/
+
+
 #pragma once
 
 #include "entity.h"
