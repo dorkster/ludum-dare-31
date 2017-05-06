5d8fbb72e05e9e64f100d7bed8d869d084a81e7f
Justin Jacobs <jajdorkster@gmail.com>
2017-05-06 14:21:06 -0400

    Hide Emscripten console

--- emscripten.sh
+++ emscripten.sh
@@ -2,4 +2,5 @@
 
 rm -f game_html.zip
 emcc src/*.cpp -O2 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 --preload-file data -o index.html
+sed -i '/#output/a display:none !important;' index.html
 zip -r game_html.zip index.html index.js index.data index.html.mem data/*
