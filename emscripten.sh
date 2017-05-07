755cbb8673221f76007d146a55ccf59ab1bc990a
Justin Jacobs <jajdorkster@gmail.com>
2017-05-07 14:25:51 -0400

    Add HTML template for Emscripten

--- emscripten.sh
+++ emscripten.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm -f game_html.zip
-emcc src/*.cpp -O2 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 --preload-file data -o index.html
-sed -i '/#output/a display:none !important;' index.html
+# -O2 is recommended here, but game fails to run
+# oddly, no optimization, -O1, and -O3 work
+emcc src/*.cpp -O3 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 --preload-file data -o index.html
+cp index_template.html index.html
 zip -r game_html.zip index.html index.js index.data index.html.mem data/*
